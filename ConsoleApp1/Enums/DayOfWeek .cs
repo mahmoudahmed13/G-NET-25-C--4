c714bc700531f67160031c3e261f98b419c3c327
mahmoudahmed13 <mahmoudahmed13@gmail.com>
2026-02-16 16:45:26 +0200

    Add project files.

---
+++ ConsoleApp1/Enums/DayOfWeek .cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Enums
+{
+    enum DaysOfWeek
+    {
+        Saturday =1 ,
+        Sunday,
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday, 
+        Friday  
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using ConsoleApp1.Enums;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write($"Enter a day number (1-7): ");
+            bool days = int.TryParse(Console.ReadLine(), out int day);
+            DaysOfWeek dayOfWeek = (DaysOfWeek)day;
+            Console.WriteLine($"Day: {dayOfWeek}");
+
+            string howDay = dayOfWeek switch
+            {
+                DaysOfWeek.Saturday or DaysOfWeek.Friday => "It's the Weekend",
+                _ => "It's a Workday"
+            };
+            Console.WriteLine(howDay);
+        }
+    }
+}
