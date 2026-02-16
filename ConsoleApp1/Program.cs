1d6f6f831cc4cda6dbfe9adfcd91a0e6a1932caf
mahmoudahmed13 <mahmoudahmed13@gmail.com>
2026-02-16 17:33:18 +0200

    Question02

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -20,6 +20,55 @@ namespace ConsoleApp1
             //};
             //Console.WriteLine(howDay); 
             #endregion
+
+            #region Part 2: Arrays
+
+            #region Question 02
+
+            //Console.Write("Please enter array size: ");
+            //int[] arr;
+            //int sum =0;
+            //int max,min;
+            //double average = 0 ;
+            //if (int.TryParse(Console.ReadLine(), out int size) && size > 0)
+            //{
+            //    arr = new int[size];
+            //    for (int i = 0; i < size; i++)
+            //    {
+            //        Console.Write($"Enter element [{i}]:");
+            //        arr[i] = int.Parse(Console.ReadLine());
+                    
+            //        sum += arr[i]; 
+            //    }
+            //    average = sum / arr.Length;
+            //    max = arr[0];
+            //    min = arr[0];
+            //    for (int j = 0; j < size; j++)
+            //    {
+            //        if (arr[j] > max)
+            //            max = arr[j];
+            //        if (arr[j] < min)
+            //            min = arr[j];
+            //    }
+
+            //    Console.WriteLine($"Sum = {sum}");
+            //    Console.WriteLine($"Average = {average}");
+            //    Console.WriteLine($"Max = {max}");
+            //    Console.WriteLine($"Min = {min}");
+
+            //    Console.Write("Reverse =");
+            //    for (int r = size - 1 ; r >= 0 ; r--)
+            //    {
+            //        Console.Write($" {arr[r]}, ");
+                     
+            //    }
+
+            //}
+
+            #endregion 
+
+
+            #endregion
         }
     }
 }
